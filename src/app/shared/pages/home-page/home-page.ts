b0c97693a27828c8233b674956a8478eba5bd69c
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-07-27 12:29:54 -0400

    Creado componente de Footer

--- src/app/shared/pages/home-page/home-page.ts
+++ src/app/shared/pages/home-page/home-page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Footer } from '../../components/footer/footer';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home-page.html',
 })
 export class HomePage {}
