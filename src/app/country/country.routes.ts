0bcdbc47595191aec7236953d0a1fcd6f8cef146
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-07-27 12:20:43 -0400

    Creado componente CountryLayout

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,8 +1,23 @@
 import { Routes } from '@angular/router';
 import { ByCapitalPage } from './pages/by-capital-page/by-capital-page';
+import { CountryLayout } from './layouts/country-layout/country-layout';
 
 export const CountryRoutes: Routes = [
-  {path: '', component: ByCapitalPage, pathMatch: 'full'}
+  {
+    path: '',
+    component: CountryLayout,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPage,
+        pathMatch: 'full'
+      },
+      {
+        path: '**',
+        redirectTo: 'by-capital'
+      }
+    ]
+  }
 ];
 
 export default CountryRoutes;
