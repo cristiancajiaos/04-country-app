13b2969ae4b0bdf7d8f55f901083223416cdf91e
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-07-28 09:21:40 -0400

    Adición de input obligatorio en componentes ByCountryPage y ByRegionPage

--- src/app/country/pages/by-country-page/by-country-page.ts
+++ src/app/country/pages/by-country-page/by-country-page.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { SearchInput } from '../../components/search-input/search-input';
 import { CountryList } from '../../components/country-list/country-list';
+import { Country } from '../../interfaces/rest-countries';
 
 @Component({
   selector: 'app-by-country-page',
@@ -8,6 +9,8 @@ import { CountryList } from '../../components/country-list/country-list';
   templateUrl: './by-country-page.html',
 })
 export class ByCountryPage {
+
+  countries = signal<Country[]>([]);
 
   receiveSearchInput(value: string): void {
     console.log({value});
--- src/app/country/pages/by-region-page/by-region-page.ts
+++ src/app/country/pages/by-region-page/by-region-page.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CountryList } from '../../components/country-list/country-list';
+import { Country } from '../../interfaces/rest-countries';
 
 @Component({
   selector: 'app-by-region-page',
   imports: [CountryList],
   templateUrl: './by-region-page.html',
 })
-export class ByRegionPage {}
+export class ByRegionPage {
+
+  countries = signal<Country[]>([]);
+}
